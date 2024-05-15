1449805b6ca817eca24be3e4b061d20770b17506
amadeus127 <nitram.reyess@hotmail.com>
2024-05-15 15:26:18 -0600

    Update main.js

--- control/web/js/main.js
+++ control/web/js/main.js
@@ -10,7 +10,7 @@ function callApi(estatus) {
   // Hacer una petición para un usuario con ID especifico
   axios
     .get(
-      "https://18.132.200.102/iot-car-control/back-end/apis/setRegistro.php?valorEstatus=" +
+      "http://18.132.200.102/iot-car-control/back-end/apis/setRegistro.php?valorEstatus=" +
         estatus
     )
     .then(function (response) {
